f38bb277cc3c602cdec72ea47d96874ace857c30
Paul Hallett <hello@phalt.co>
2014-10-23 15:17:22 -0700

    Updating static files

--- poketext/settings.py
+++ poketext/settings.py
@@ -82,6 +82,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
 
 DATABASES['default'] = dj_database_url.config()
