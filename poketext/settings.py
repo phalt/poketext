335ca4b19efebed0afffc3a03484da1f6e85e8a8
Paul Hallett <hello@phalt.co>
2014-10-23 14:50:07 -0700

    Adding in dj_database_url to settings

--- poketext/settings.py
+++ poketext/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -81,3 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATABASES['default'] = dj_database_url.config()
