c115c2d3c403165b9287df0865705fff7a76d338
Paul Hallett <hello@phalt.co>
2014-10-23 15:21:59 -0700

    Typo in settings.py

--- poketext/settings.py
+++ poketext/settings.py
@@ -86,7 +86,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 DATABASES['default'] = dj_database_url.config()
