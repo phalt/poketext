112a5f368e7bc6449eb29e43a3452aa4d6c2a005
Paul Hallett <hello@phalt.co>
2014-10-27 18:36:34 -0700

    Deploy live database

--- poketext/settings.py
+++ poketext/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '+sbtc&fs1v^#g451otynn1iop76_y5+i48n67u=d_nx1&m+i-)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
